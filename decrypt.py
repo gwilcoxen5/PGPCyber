7162d859c4b77bcc1a269577eb2cbb4b79dd7d1c
gwilcoxen5 <gwilcoxen5@gmail.com>
2025-10-22 12:57:00 -0500

    Update decrypt.py

--- decrypt.py
+++ decrypt.py
@@ -1,7 +1,7 @@
 import pgpy
 
 # This loads private key
-privkey, _ = pgpy.PGPKey.from_file('private.asc')
+privkey, _ = pgpy.PGPKey.from_file('private_key.asc')
 privkey.unlock("")  
 # This doesn't have password protection but we can add this later if we want
 # We just have to set for password protection and this will be what is used to unlock (figure this out later, working on this)
